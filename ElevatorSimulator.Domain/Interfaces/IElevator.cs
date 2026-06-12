b83a051a10cee82d8a8b1adebf9109e6edbdabf4
Your Name <NRankapole@dvtsoftware.com>
2026-06-12 14:40:21 +0200

    feat: Create IElevator interface

---
+++ ElevatorSimulator.Domain/Interfaces/IElevator.cs
@@ -0,0 +1,17 @@
+namespace ElevatorSimulator.Domain;
+
+using ElevatorSimulator.Domain.Enums;
+
+public interface IElevator
+{
+    int ElevatorId { get; }
+    int CurrentFloor { get; }
+    int PassengerCount { get; }
+    int MaxCapacity { get; }
+    bool IsAtMaxCapacity { get; }
+    ElevatorDirection Direction { get; }
+    ElevatorState State { get; }
+
+    void pickUpPassengers(int count);
+    void dropOffPassengers(int count);
+}
