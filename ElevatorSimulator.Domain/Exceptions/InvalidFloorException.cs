d7014debd3b2aee71f2a99b3e68b945e33f7146d
Your Name <NRankapole@dvtsoftware.com>
2026-06-14 19:42:21 +0200

    refactor: Improve exception message

--- ElevatorSimulator.Domain/Exceptions/InvalidFloorException.cs
+++ ElevatorSimulator.Domain/Exceptions/InvalidFloorException.cs
@@ -1,4 +1,4 @@
 namespace ElevatorSimulator.Domain.Exceptions;
 
 public sealed class InvalidFloorException(int selectedFloor, int minFloorAvailable, int maxFloorAvailable):
-    Exception($"The floor you have selected '{selectedFloor}' is invalid. Select a floor from {minFloorAvailable}-{maxFloorAvailable}");
+    Exception($"Floor {selectedFloor} is invalid. Select a floor from {minFloorAvailable}-{maxFloorAvailable}");
