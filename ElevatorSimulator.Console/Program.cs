2bf8e55ee90cf1f8ddedda8c64f5418db3c4c31f
Your Name <NRankapole@dvtsoftware.com>
2026-06-18 11:22:51 +0200

    chore: Wire up application dependencies

--- ElevatorSimulator.Console/Program.cs
+++ ElevatorSimulator.Console/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ElevatorSimulator.Application.Interfaces;
+using ElevatorSimulator.Application.Services;
+using ElevatorSimulator.Application.Strategies;
+using ElevatorSimulator.Console.Setup;
+using Microsoft.Extensions.DependencyInjection;
+
+var buildingConfig = BuildingConfigurator.Configure();
+
+var services = new ServiceCollection();
+services.AddSingleton<IDispatchStrategy, NearestElevatorStrategy>();
+services.AddSingleton(buildingConfig.Elevators);
+services.AddSingleton<ElevatorController>();
+services.AddSingleton(_ => new FloorManager(buildingConfig.MinFloor, buildingConfig.MaxFloor));
+
+var provider = services.BuildServiceProvider();
+var elevatorController = provider.GetRequiredService<ElevatorController>();
+var floorManager = provider.GetRequiredService<FloorManager>();
+
+
