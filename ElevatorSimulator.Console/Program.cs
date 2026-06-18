228d821ca707da2bfe5cef4a7bd9812379effa32
Your Name <NRankapole@dvtsoftware.com>
2026-06-18 14:08:25 +0200

    feat: Implement ConsoleRenderer and InputHandler (full working system)

--- ElevatorSimulator.Console/Program.cs
+++ ElevatorSimulator.Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ElevatorSimulator.Application.Interfaces;
 using ElevatorSimulator.Application.Services;
 using ElevatorSimulator.Application.Strategies;
+using ElevatorSimulator.Console.Input;
+using ElevatorSimulator.Console.Rendering;
 using ElevatorSimulator.Console.Setup;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,4 +18,21 @@ var provider = services.BuildServiceProvider();
 var elevatorController = provider.GetRequiredService<ElevatorController>();
 var floorManager = provider.GetRequiredService<FloorManager>();
 
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, elevator) =>
+{
+    elevator.Cancel = true;
+    cts.Cancel();
+};
 
+var statusLineCount = ConsoleRenderer.LineCount(buildingConfig.Elevators.Count);
+var inputHandler = new InputHandler(elevatorController, floorManager, inputStartRow: statusLineCount + 1);
+
+await Task.WhenAll(
+    ConsoleRenderer.RenderLoop(elevatorController.Elevators, cts.Token),
+    inputHandler.InputLoop(cts.Token)
+);
+
+Console.SetCursorPosition(0, statusLineCount + 6);
+Console.WriteLine("Simulation ended. Press any key to exit.");
+Console.ReadKey(intercept: true);
